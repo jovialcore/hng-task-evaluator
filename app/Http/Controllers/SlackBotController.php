beac5cb2958b67453f60a13ff262b0fc8accabfc
Neo Ighodaro <807318+neoighodaro@users.noreply.github.com>
2022-10-29 00:46:27 +0200

    Testing success response of slackbot

--- app/Http/Controllers/SlackBotController.php
+++ app/Http/Controllers/SlackBotController.php
@@ -4,14 +4,67 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use GuzzleHttp\RequestOptions;
 
 final class SlackBotController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, Client $client)
     {
-        error_log(json_encode($request->all()));
+        $responseUrl = $request->get('response_url');
 
-        return 'ok';
+        $successful = true;
+        $errors = [];
+
+        $client->post($responseUrl, [
+            RequestOptions::JSON => $successful ? $this->successMessage() : $this->failureMessage($errors),
+        ]);
+
+        return '';
+    }
+
+    protected function successMessage(): array
+    {
+        return [
+            'response_type' => 'ephemeral',
+            'blocks' => [
+                [
+                    'type' => 'section',
+                    'text' => [
+                        'emoji' => true,
+                        'type' => 'plain_text',
+                        'text' => '✅ Your task was validated and submitted! Congrats!',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    protected function failureMessage(array $errors): array
+    {
+        return [
+            'response_type' => 'ephemeral',
+            'blocks' => [
+                [
+                    'type' => 'section',
+                    'text' => [
+                        'emoji' => true,
+                        'type' => 'plain_text',
+                        'text' => '❌ Your task verification failed. Sorry.',
+                    ],
+                ],
+                [
+                    'type' => 'divider',
+                ],
+                [
+                    'type' => 'section',
+                    'text' => [
+                        'type' => 'mrkdwn',
+                        'text' => implode("\n", array_map(static fn (string $error) => "- {$error}", $errors)),
+                    ],
+                ],
+            ],
+        ];
     }
 }
