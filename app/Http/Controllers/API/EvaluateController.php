da311a91ad179848bf305eb3054a01e4f72e8b1f
Neo Ighodaro <807318+neoighodaro@users.noreply.github.com>
2022-10-28 17:09:35 +0200

    Small updates

--- app/Http/Controllers/API/EvaluateController.php
+++ app/Http/Controllers/API/EvaluateController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Controllers\Stage1\API;
+namespace App\Http\Controllers\API;
 
 use App\Validator;
 use App\Service\Stage1;
@@ -10,9 +10,15 @@ use Illuminate\Http\Request;
 use App\Service\EvaluateService;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
+use App\Service\Contracts\Evaluator;
 
 final class EvaluateController extends Controller
 {
+    private function evaluator(): Evaluator
+    {
+        return new Stage1\Evaluator();
+    }
+
     public function __invoke(Request $request, EvaluateService $evaluateService, Validator $validator): JsonResponse
     {
         $urls = $validator->validate($request->all(), [
@@ -20,7 +26,7 @@ final class EvaluateController extends Controller
             'urls.*' => 'required|url',
         ])['urls'];
 
-        $evaluateService->evaluate(array_unique($urls), new Stage1\Evaluator());
+        $evaluateService->evaluate(array_unique($urls), $this->evaluator());
 
         return new JsonResponse([
             'data' => [
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,5 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\Stage1;
-
 /** @var \Illuminate\Routing\Router $router */
-$router->post('/stage1/evaluate', Stage1\API\EvaluateController::class);
+$router->post('/backend/evaluate', \App\Http\Controllers\API\EvaluateController::class);
