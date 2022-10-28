fde45ba2528b0f525edd5913be243c1ed0c48b2f
Neo Ighodaro <807318+neoighodaro@users.noreply.github.com>
2022-10-29 00:55:31 +0200

    Test failure

--- app/Http/Controllers/SlackBotController.php
+++ app/Http/Controllers/SlackBotController.php
@@ -4,17 +4,24 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Validator;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use GuzzleHttp\RequestOptions;
 
 final class SlackBotController extends Controller
 {
-    public function __invoke(Request $request, Client $client)
+    public function __invoke(Request $request, Client $client, Validator $validator)
     {
+        $text = $request->get('text');
+        $submittedUrl = preg_match('/https?:\/\/[^\s]+/', $text, $matches) ? $matches[0] : null;
+
+        $url = $validator->validate(['url' => $submittedUrl], ['url' => 'required|url'])['url'];
+
+        $slackUsername = $request->get('user_name');
         $responseUrl = $request->get('response_url');
 
-        $successful = true;
+        $successful = false;
         $errors = [];
 
         $client->post($responseUrl, [
@@ -34,7 +41,7 @@ final class SlackBotController extends Controller
                     'text' => [
                         'emoji' => true,
                         'type' => 'plain_text',
-                        'text' => '✅ Your task was validated and submitted! Congrats!',
+                        'text' => '🎉🥳 Your task was validated and submitted! Congrats!',
                     ],
                 ],
             ],
