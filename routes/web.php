dab59c2713621f6f43c21039d93f949ae51f75de
Neo Ighodaro <807318+neoighodaro@users.noreply.github.com>
2022-10-29 14:09:58 +0200

    Add possibility to download the passed users

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,22 @@
 
 declare(strict_types=1);
 
+use Illuminate\Http\Response;
 use App\Http\Controllers\SlackBotController;
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 
 /** @var \Illuminate\Routing\Router $router */
 $router->post('/slackbot', SlackBotController::class);
+
+$router->get('/passed', function () {
+    return new Response(
+        file_get_contents(PROJECT_ROOT_PATH.'/storage/passed.csv'),
+        HttpFoundationResponse::HTTP_OK,
+        [
+            'Content-Type' => 'text/csv',
+            'Content-Disposition' => 'attachment; filename="passed.csv"',
+        ]
+    );
+});
+
 $router->get('/', fn () => 'Hello World');
