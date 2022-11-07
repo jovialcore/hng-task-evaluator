8ff48aef443c37b97ec6bfd84632b618afadde7e
Chidiebere Chukwudi <jollydosky@gmail.com>
2022-11-07 08:50:42 +0100

    Deadline closure

--- app/Http/Controllers/SlackBotController.php
+++ app/Http/Controllers/SlackBotController.php
@@ -77,8 +77,8 @@ final class SlackBotController extends Controller
 
     protected function stageHasEnded(int $stage): bool
     {
-                return $stage === 1;
-            //return in_array($stage, [1, 2]);
+//                 return $stage === 1;
+            return in_array($stage, [1, 2]);
     }
 
     protected function csvAdditionalData(Request $request): array
