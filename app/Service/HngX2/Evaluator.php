18fb82fefb407699c047a76dfc74bb69bb2d8fd5
chidiebere chukwudi <chidideveloper@gmail.com>
2023-09-08 19:37:38 +0100

    stage two

--- app/Service/HngX2/Evaluator.php
+++ app/Service/HngX2/Evaluator.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace App\Service\HngX2;
 
+use App\Service\Concerns\HandlesAndProvidesData;
+use GuzzleHttp\Promise\Utils;
+
 class Evaluator
 {
+
+    use HandlesAndProvidesData {
+        data as protected traitData;
+        getContent as protected traitGetContent;
+    }
     public function rules(string $url): array
     {
         return [
@@ -13,4 +21,29 @@ class Evaluator
             'name' => ['required', 'string'],
         ];
     }
+    public function post($url)
+    {
+
+        $formData = [
+            'name' => 'Elon Musk'
+        ];
+        $promises = [
+            'url' => $this->http()->postAsync($url, $formData)
+        ];
+
+        $response = Utils::settle($promises)->wait();
+
+        dd($response['url']);
+    }
+
+    // make a post request to their app
+
+    // by adding it to the body
+
+    // return a fulfilled state
+
+    // if fulfilled state, now make a read request to that endpoint
+
+
+
 }
